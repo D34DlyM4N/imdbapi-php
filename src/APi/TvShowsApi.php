9b91283d2aa26f374a7127d83f23b199bb8e8775
D34DlyM4N <d34dlym4n@gmail.com>
2026-04-18 18:23:22 +0300

    fix api

--- src/APi/TvShowsApi.php
+++ src/APi/TvShowsApi.php
@@ -1,6 +1,25 @@
-private $client;
+<?php
 
-public function __construct(ApiClient $client)
+namespace ImdbApi\Api;
+
+use ImdbApi\Http\ApiClient;
+
+class TvShowsApi
 {
-    $this->client = $client;
+    private $client;
+
+    public function __construct(ApiClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function get(string $id): array
+    {
+        return $this->client->request('GET', "/tvshows/{$id}");
+    }
+
+    public function popular(): array
+    {
+        return $this->client->request('GET', "/tvshows/popular");
+    }
 }
